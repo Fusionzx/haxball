611a73155f5fd7877f9feb02eb52596138bb7f60
Fusion <74070053+Fusionzx@users.noreply.github.com>
2026-05-30 01:33:07 -0300

    Update __init__.py exports for extended modules

--- haxball_py/__init__.py
+++ haxball_py/__init__.py
@@ -3,10 +3,19 @@ from .config import GeoConfig, HaxballConfig
 from .events import EventEmitter
 from .models import (
     DiscProperties,
-    Player,
+    Player as NativePlayer,
     Position,
     Scores,
 )
+
+# Extended imports
+from .extended import RoomExtended, HaxballClientExtended
+from .player import Player
+from .player_list import PlayerList
+from .disc import AbstractDisc, Disc
+from .command import Command, CommandExecInfo, CommandArgument
+from .module import Module, module, module_command, event, custom_event
+from .enums import Teams, ChatStyle, ChatSounds, Stadiums, Colors
 
 __all__ = [
     "DiscProperties",
@@ -14,7 +23,28 @@ __all__ = [
     "GeoConfig",
     "HaxballClient",
     "HaxballConfig",
-    "Player",
+    "NativePlayer",
     "Position",
     "Scores",
+    
+    # Extended exports
+    "RoomExtended",
+    "HaxballClientExtended",
+    "Player",
+    "PlayerList",
+    "AbstractDisc",
+    "Disc",
+    "Command",
+    "CommandExecInfo",
+    "CommandArgument",
+    "Module",
+    "module",
+    "module_command",
+    "event",
+    "custom_event",
+    "Teams",
+    "ChatStyle",
+    "ChatSounds",
+    "Stadiums",
+    "Colors",
 ]
